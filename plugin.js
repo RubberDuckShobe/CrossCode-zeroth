2327468addcf130d9f43dd2ffa06c96acd8ee2b5
Vlams <flvlams@gmail.com>
2024-04-01 01:36:17 +0200

    yeah

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 export default class Zeroth {
-  let history = {};
-  let time = 0;
+  var history = {};
+  var time = 0;
   prestart() {
     sc.MessageModel.inject({
       showMessage(person, text, someAutoContinueThing) {
@@ -215,9 +215,9 @@ export default class Zeroth {
 						"guy making this either has a life expentancy of 28 years, or he's like the number one on the 2024 MDVA groomer power ranking",
             "why you, as a man, be shoppin",
           ];
-	  let index = Math.floor(Math.random() * zerothQuotes.length);
+	  var index = Math.floor(Math.random() * zerothQuotes.length);
           while (history[index] == null || history[index] < time - 50) {
-	    let index = Math.floor(Math.random() * zerothQuotes.length);
+	    index = Math.floor(Math.random() * zerothQuotes.length);
 	  }
           history[index] = time;
 	  time += 1;
